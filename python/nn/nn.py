a407a15e6970157b3033742b0ae64c5cdc55521d
Kevin DeMarco <kevin.demarco@gtri.gatech.edu>
2016-11-15 17:47:25 -0500

    nn example

---
+++ python/nn/nn.py
@@ -0,0 +1,45 @@
+import numpy as np
+import sys
+
+# sigmoid function
+def nonlin(x,deriv=False):
+    if(deriv==True):
+        return x*(1-x)
+    return 1/(1+np.exp(-x))
+    
+# input dataset
+X = np.array([  [0,0,1],
+                [0,1,1],
+                [1,0,1],
+                [1,1,1] ])
+    
+# output dataset            
+y = np.array([[0,0,1,1]]).T
+
+# seed random numbers to make calculation
+# deterministic (just a good practice)
+np.random.seed(1)
+
+# initialize weights randomly with mean 0
+syn0 = 2*np.random.random((3,1)) - 1
+#syn0 = np.array([[1.],[2.],[3.]])
+
+for iter in xrange(10000):
+
+    # forward propagation
+    l0 = X
+    dot = np.dot(l0,syn0)
+    l1 = nonlin(dot)
+        
+    # how much did we miss?
+    l1_error = y - l1
+        
+    # multiply how much we missed by the 
+    # slope of the sigmoid at the values in l1
+    l1_delta = l1_error * nonlin(l1,True)
+    
+    # update weights
+    syn0 += np.dot(l0.T,l1_delta)
+
+print "Output After Training:"
+print l1
