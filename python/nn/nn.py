9cd67f11442489a7eabf672249a68b8477f70c3b
Kevin DeMarco <kevin.demarco@gtri.gatech.edu>
2016-12-12 16:24:03 -0500

    tf tutorial

--- python/nn/nn.py
+++ python/nn/nn.py
@@ -6,40 +6,45 @@ def nonlin(x,deriv=False):
     if(deriv==True):
         return x*(1-x)
     return 1/(1+np.exp(-x))
+
+def main():    
+    # input dataset
+    X = np.array([  [0,0,1],
+                    [0,1,1],
+                    [1,0,1],
+                    [1,1,1] ])
     
-# input dataset
-X = np.array([  [0,0,1],
-                [0,1,1],
-                [1,0,1],
-                [1,1,1] ])
-    
-# output dataset            
-y = np.array([[0,0,1,1]]).T
+    # output dataset            
+    y = np.array([[0,0,1,1]]).T
 
-# seed random numbers to make calculation
-# deterministic (just a good practice)
-np.random.seed(1)
+    # seed random numbers to make calculation
+    # deterministic (just a good practice)
+    np.random.seed(1)
 
-# initialize weights randomly with mean 0
-syn0 = 2*np.random.random((3,1)) - 1
-#syn0 = np.array([[1.],[2.],[3.]])
+    # initialize weights randomly with mean 0
+    syn0 = 2*np.random.random((3,1)) - 1
+    #syn0 = np.array([[1.],[2.],[3.]])
 
-for iter in xrange(10000):
-
-    # forward propagation
-    l0 = X
-    dot = np.dot(l0,syn0)
-    l1 = nonlin(dot)
+    for iter in xrange(10000):
+        # forward propagation
+        l0 = X
+        dot = np.dot(l0,syn0)
+        l1 = nonlin(dot)
         
-    # how much did we miss?
-    l1_error = y - l1
+        # how much did we miss?
+        l1_error = y - l1
         
-    # multiply how much we missed by the 
-    # slope of the sigmoid at the values in l1
-    l1_delta = l1_error * nonlin(l1,True)
+        # multiply how much we missed by the 
+        # slope of the sigmoid at the values in l1
+        l1_delta = l1_error * nonlin(l1,True)
     
-    # update weights
-    syn0 += np.dot(l0.T,l1_delta)
+        # update weights
+        syn0 += np.dot(l0.T,l1_delta)
 
-print "Output After Training:"
-print l1
+    print "Output After Training:"
+    print l1
+
+if __name__ == '__main__':
+    main()
+
+
